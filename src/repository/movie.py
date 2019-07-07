06043181e2f7295d5c9f3ade2fbf09135a5b9362
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-07 20:50:39 +0600

    Rename MovieRepository class method from get_complete_movie_info to get_movie_info

--- src/repository/movie.py
+++ src/repository/movie.py
@@ -7,7 +7,7 @@ from ..redis_utilities import RedisUtilities
 class MovieRepository(object):
 
     @staticmethod
-    def get_complete_movie_info(imdb_id: str) -> Dict[str, Union[str, int, List, Dict]]:
+    def get_movie_info(imdb_id: str) -> Dict[str, Union[str, int, List, Dict]]:
         cached_movie_data = RedisUtilities.get_item(imdb_id)
         if not cached_movie_data:
             omdb_client = OMDBClient()
