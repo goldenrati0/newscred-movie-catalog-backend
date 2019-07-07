3976710a7085d66ff580eea9eb704efe7c62a611
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-07 20:49:36 +0600

    Add initial method for MovieRepository class

---
+++ src/repository/movie.py
@@ -0,0 +1,17 @@
+from typing import Dict, List, Union
+
+from ..models import OMDBClient
+from ..redis_utilities import RedisUtilities
+
+
+class MovieRepository(object):
+
+    @staticmethod
+    def get_complete_movie_info(imdb_id: str) -> Dict[str, Union[str, int, List, Dict]]:
+        cached_movie_data = RedisUtilities.get_item(imdb_id)
+        if not cached_movie_data:
+            omdb_client = OMDBClient()
+            movie_data = omdb_client.get_full_movie_info(imdb_id=imdb_id)
+            RedisUtilities.set_item(key=imdb_id, data=movie_data)
+            return movie_data
+        return cached_movie_data
