953770af783a03fe7c2ab703eec465df2a3babdf
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-08 15:46:33 +0600

    Add UserToken repository class

---
+++ src/repository/user_token.py
@@ -0,0 +1,14 @@
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+from ..models import User
+
+
+class UserToken:
+
+    @staticmethod
+    def create_user_access_token(user: User) -> str:
+        return create_access_token(identity=user.id)
+
+    @staticmethod
+    def create_user_refresh_token(user: User) -> str:
+        return create_refresh_token(identity=user.id)
