baf8500a49d38e49b35588afded85704d2fcadd6
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-08 16:47:38 +0600

    :fire: Remove jwt definitions from flask_setup, Create separate file for jwt callbacks

--- src/core/flask_app.py
+++ src/core/flask_app.py
@@ -8,17 +8,3 @@ app = Flask(__name__)
 CORS(app)
 app.config.from_object(Configuration)
 jwt = JWTManager(app)
-
-from ..models import User
-from ..repository import UserRepository
-from ..utils import ResponseGenerator
-
-
-@jwt.user_loader_callback_loader
-def flask_jwt_user_loader_callback(identity) -> User:
-    return UserRepository.get_by_id(id=identity)
-
-
-@jwt.user_loader_error_loader
-def flask_jwt_user_loader_error_callback(identity):
-    return ResponseGenerator.not_found(msg="User::{} not found".format(identity))
