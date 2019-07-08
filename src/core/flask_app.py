59ae17b3eb75f7b6eb215836d2b63e47324fab74
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-08 16:30:59 +0600

    :bento: Add flask-jwt user loader error callback

--- src/core/flask_app.py
+++ src/core/flask_app.py
@@ -11,8 +11,14 @@ jwt = JWTManager(app)
 
 from ..models import User
 from ..repository import UserRepository
+from ..utils import ResponseGenerator
 
 
 @jwt.user_loader_callback_loader
 def flask_jwt_user_loader_callback(identity) -> User:
     return UserRepository.get_by_id(id=identity)
+
+
+@jwt.user_loader_error_loader
+def flask_jwt_user_loader_error_callback(identity):
+    return ResponseGenerator.not_found(msg="User::{} not found".format(identity))
