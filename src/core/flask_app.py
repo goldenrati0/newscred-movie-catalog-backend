786fa4a883270cfa999d76f9a02a16db54cffdab
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-08 15:43:07 +0600

    Add flask-jwt user loader callback

--- src/core/flask_app.py
+++ src/core/flask_app.py
@@ -8,3 +8,11 @@ app = Flask(__name__)
 CORS(app)
 app.config.from_object(Configuration)
 jwt = JWTManager(app)
+
+from ..models import User
+from ..repository import UserRepository
+
+
+@jwt.user_loader_callback_loader
+def flask_jwt_user_loader_callback(identity) -> User:
+    return UserRepository.get_by_id(id=identity)
