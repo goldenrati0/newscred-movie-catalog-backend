2f5187b394e271b42b91191d586b974ce7ebb1a3
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-08 17:00:10 +0600

    :bento: Add json data verification decorator

---
+++ src/decorators/requests.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+from flask import request
+
+from ..utils import ResponseGenerator
+
+
+def json_data_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if not request.is_json:
+            return ResponseGenerator.json_data_expected()
+
+        try:
+            request.get_json()
+        except Exception as _e:
+            return ResponseGenerator.json_data_expected()
+
+        return f(*args, **kwargs)
+
+    return wrap
