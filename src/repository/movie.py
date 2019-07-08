61d2160fa37796d49cee40e3ffffc66b10706e69
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-08 15:31:56 +0600

    Change MovieRepository redis client creation using RedisCacheFactory

--- src/repository/movie.py
+++ src/repository/movie.py
@@ -1,17 +1,19 @@
 from typing import Dict, List, Union
 
 from ..models import OMDBClient
-from ..redis_utilities import RedisUtilities
+from ..redis_utilities import RedisCacheFactory
+
+redis_movie_cache = RedisCacheFactory.get_redis_client(storage="movie")
 
 
 class MovieRepository(object):
 
     @staticmethod
     def get_movie_info(imdb_id: str) -> Dict[str, Union[str, int, List, Dict]]:
-        cached_movie_data = RedisUtilities.get_item(imdb_id)
+        cached_movie_data = redis_movie_cache.get(imdb_id)
         if not cached_movie_data:
             omdb_client = OMDBClient()
             movie_data = omdb_client.get_full_movie_info(imdb_id=imdb_id)
-            RedisUtilities.set_item(key=imdb_id, data=movie_data)
+            redis_movie_cache.set(key=imdb_id, value=movie_data)
             return movie_data
         return cached_movie_data
