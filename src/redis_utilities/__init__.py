269a0ec6405d60c1657916fd246c00903e55ff8b
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-08 15:30:49 +0600

    Create redis client factory, Add abstraction on client creation

--- src/redis_utilities/__init__.py
+++ src/redis_utilities/__init__.py
@@ -1,24 +1,66 @@
 import pickle
-from typing import Optional, Dict
 
 from redis import Redis
 
 from ..core.config import Configuration
 
-RDS_CLIENT = Redis(host=Configuration.REDIS_HOST, port=Configuration.REDIS_PORT, db=Configuration.REDIS_DB,
-                   password=Configuration.REDIS_PASSWORD)
 DEFAULT_EXPIRATION_TIME = 10 * 24 * 60 * 60  # 10 days
 
 
-class RedisUtilities:
+class CacheBase:
 
-    @staticmethod
-    def get_item(key: str) -> Optional[Dict]:
-        data = RDS_CLIENT.get(name=key)
-        if not data:
+    def __init__(self, host, port, db, password, **kwargs):
+        self.client = Redis(host=host, port=port, db=db, password=password, **kwargs)
+        self.ex = kwargs.get("ex", DEFAULT_EXPIRATION_TIME)
+
+    def set(self, key, value, **kwargs) -> bool:
+        raise NotImplementedError
+
+    def get(self, key):
+        raise NotImplementedError
+
+
+class RedisCache(CacheBase):
+
+    def __init__(self, db: int = 0):
+        super(RedisCache, self).__init__(Configuration.REDIS_HOST, Configuration.REDIS_PORT, db,
+                                         Configuration.REDIS_PASSWORD)
+
+    def set(self, key, value, **kwargs) -> bool:
+        return self.client.set(name=key, value=pickle.dumps(value), ex=kwargs.get("ex", self.ex))
+
+    def get(self, key):
+        obj = self.client.get(name=key)
+        if not obj:
             return None
-        return pickle.loads(data)
+        return pickle.loads(obj)
+
+
+class RedisUserCache(RedisCache):
+
+    def __init__(self):
+        super(RedisUserCache, self).__init__(db=0)
+
+
+class RedisMovieCache(RedisCache):
+    client = None
+
+    def __init__(self):
+        super(RedisMovieCache, self).__init__(db=1)
+
+
+class RedisCacheFactory:
+    clients = {}
 
     @staticmethod
-    def set_item(key: str, data) -> bool:
-        return RDS_CLIENT.set(name=key, value=pickle.dumps(data), ex=DEFAULT_EXPIRATION_TIME)
+    def get_redis_client(storage: str) -> RedisCache:
+        if storage not in RedisCacheFactory.clients:
+            c = None
+            if storage == "user":
+                c = RedisUserCache()
+            elif storage == "movie":
+                c = RedisMovieCache()
+            if c:
+                RedisCacheFactory.clients[storage] = c
+
+        return RedisCacheFactory.clients[storage]
