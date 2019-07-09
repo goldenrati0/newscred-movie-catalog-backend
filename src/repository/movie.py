4ac343780b28ba90da91f53d79c03e90a8b4e73a
Tahmid Choyon <tahmid.choyon@gmail.com>
2019-07-09 10:30:54 +0600

    :sparkles: Add ShortPlot in movie info, Truncating plot till 50 characters

--- src/repository/movie.py
+++ src/repository/movie.py
@@ -15,6 +15,7 @@ class MovieRepository(object):
         cached_movie_data = MovieRepository.redis_movie_cache.get(movie.imdb_id)
         if not cached_movie_data:
             movie_data = MovieRepository.omdb_client.get_full_movie_info(imdb_id=movie.imdb_id)
+            movie_data["ShortPlot"] = movie_data.get("Plot", "N/A")[:50]
             MovieRepository.redis_movie_cache.set(key=movie.imdb_id, value=movie_data)
             cached_movie_data = movie_data
         return cached_movie_data
